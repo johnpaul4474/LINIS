a28cdf8ad1d0f8b851faf4330373cc1376c8666f
DOT28 <jbaquino3@up.edu.ph>
2022-08-05 15:13:02 +0800

    wards and offices viewing of issued products

--- app/Http/Controllers/Department/DepartmentController.php
+++ app/Http/Controllers/Department/DepartmentController.php
@@ -14,4 +14,61 @@ class DepartmentController extends Controller {
 	public static function officeList() { 
 		return Office::all();
 	}
+
+	public function index() {
+		$wards = $this->wardList();
+		$offices = $this->officeList();
+
+		return view("departments.departmentsList", compact('wards', 'offices'));
+	}
+
+	public function wardIssuedProducts($ward_id) {
+		$name = Ward::find($ward_id)->ward_name;
+		$type = "(WARD)";
+
+		$products = \DB::select("
+			select
+				product_stock_id,
+				product_unit,
+				product_quantity,
+				product_unit_cost,
+				total_cost,
+				product_name,
+				case
+					when is_condemned=1 then 'Condemned'
+					when is_lossed=1 then 'Lost'
+					when is_returned=1 then 'Returned'
+					else 'Issued'
+				end as status
+			from nora.paul.linen_products_list
+			where issued_ward_id = ?
+		", [$ward_id]);
+
+		return view("departments.issuedProducts", compact('products', 'name', 'type'));
+	}
+
+	public function officeIssuedProducts($office_id) {
+		$name = Office::find($office_id)->office_name;
+		$type = "(OFFICE)";
+		
+		$products = \DB::select("
+			select
+				product_stock_id,
+				product_unit,
+				product_quantity,
+				product_unit_cost,
+				total_cost,
+				product_name,
+				case
+					when is_condemned=1 then 'Condemned'
+					when is_lossed=1 then 'Lost'
+					when is_returned=1 then 'Returned'
+					else 'Issued'
+				end as status
+			from nora.paul.linen_products_list
+			where issued_office_id = ?
+		", [$office_id]);
+
+		return view("departments.issuedProducts", compact('products', 'name', 'type'));
+	}
 }
---
+++ resources/views/departments/issuedProducts.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.appHome')
+
+@section('content')
+<div class="row">    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-header text-white" style="background-color: #00AA9E;">{{$name}} {{$type}}</div>
+                    <div class="card-body">
+                        <table id="wardofficeTable" class="table  table-bordered table-success" style="width:100%">
+                            <thead>
+                                    <th>Product Stock ID</th>
+                                    <th>Product Name</th>
+                                    <th>Quantity</th>
+                                    <th>Unit Cost</th>
+                                    <th>Total Cost</th>
+                                    <th>Status</th>
+                                    
+                            </thead> 
+                            <tbody>
+                                @foreach ($products as $product)
+                                <tr>
+                                    <td>{{$product->product_stock_id}} </td>
+                                    <td>{{$product->product_name}} </td>
+                                    <td>{{$product->product_quantity}} {{$product->product_unit}}/S</td>
+                                    <td>{{$product->product_unit_cost}} </td>
+                                    <td>{{$product->total_cost}} </td>
+                                    <td>{{$product->status}} </td>
+                                </tr>   
+                                @endforeach
+                            </tbody>       
+                    </table>  
+                    </div>    
+                </div>         
+            </div>
+        </div>    
+      </div> 
+    </div>
+</div>   
+@endSection
+@push('scripts')
+
+<script>
+$(document).ready(function () {
+
+
+  $('#wardofficeTable').DataTable({
+            "lengthMenu": [[5, 25, 50, -1], [5, 25, 50, "All"]],
+            "search": true,     
+            // "ordering": false,
+            "order": [[ 1, "asc" ]],
+            "paging": true,
+            "pageLength": 10,
+            "columnDefs": [
+                            {
+                                "targets": [ 0 ],
+                                "visible": false,
+                                "searchable": false,
+                                
+                            },          
+                        ],
+            "scrollY":        "500px",
+            "scrollX":        true,
+            "scrollCollapse": true,
+            "fixedColumns":   {
+                left:0,
+                right: 2
+            }    
+    }); 
+
+
+
+})
+     
+</script>
+@endpush
