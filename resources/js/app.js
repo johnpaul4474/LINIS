04357a68041baad2ff7809cd167ac0e054a48ed9
DOT28 <jbaquino3@up.edu.ph>
2022-08-08 08:29:37 +0800

    init vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+require('./bootstrap')
+
+import { createApp } from 'vue'
+import StockRoom from './components/StockRoom'
+
+const app = createApp({})
+
+app.component('stock-room', StockRoom)
+
+app.mount('#app')
