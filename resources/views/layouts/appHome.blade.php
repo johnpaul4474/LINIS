79805b848e205789e60d1b260b96d301bd7a7766
DOT28 <jbaquino3@up.edu.ph>
2022-08-08 09:22:42 +0800

    change logout to GET

--- resources/views/layouts/appHome.blade.php
+++ resources/views/layouts/appHome.blade.php
@@ -278,15 +278,9 @@
                                     <a class="dropdown-item" href="/password">
                                        Change Password
                                     </a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                    <a class="dropdown-item" href="/logout">
+                                        Logout
                                     </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
                                 </div>
                             </li>
                         @endguest
